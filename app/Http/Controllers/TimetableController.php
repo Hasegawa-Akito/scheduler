da6b72a0ca73560dfda0154c86ae92019a24256c
Hasegawa-Akito <h.akito1999@gmail.com>
2022-05-06 23:39:25 +0900

    読みやすいように修正

--- app/Http/Controllers/TimetableController.php
+++ app/Http/Controllers/TimetableController.php
@@ -12,67 +12,67 @@ use App\Models\Room;
 
 class TimetableController extends Controller
 {
-    public function timetable_index(Request $request,$view_user_id,$room_id,$display_date){
+    public function timetable_index(Request $request, $view_user_id, $room_id,$display_date){
 
-        $session_user_id=$request->session()->get('user_id');
-        $session_room_id=$request->session()->get('room_id');
+        $session_user_id = $request->session()->get('user_id');
+        $session_room_id = $request->session()->get('room_id');
         //dd($session_room_id);
 
-        if($room_id!=$session_room_id){
+        if($room_id != $session_room_id){
             return redirect(url('/roomlogin'));
         }
         //sessionにユーザーid追加
-        $request->session()->put('user_id',$session_user_id);
-        $request->session()->put('room_id',$session_room_id);
+        $request->session()->put('user_id', $session_user_id);
+        $request->session()->put('room_id', $session_room_id);
 
         
 
-        $user=new User;
-        $user_id_serch=$user->user_id_serch($view_user_id);
+        $user = new User;
+        $user_id_serch = $user->user_id_serch($view_user_id);
         if(!isset($user_id_serch)){
             return redirect(url('/roomlogin'));
         }
-        $view_username=$user_id_serch->username;
+        $view_username = $user_id_serch->username;
 
         
-        if($display_date!="today"){
-            $date=$display_date;
+        if($display_date != "today"){
+            $date = $display_date;
         }
         else{
-            $date= date('Y-m-d');
+            $date = date('Y-m-d');
             
         }
         //dd($date);
         
-        $timetable_create=new TimetableCreate;
-        $timetable_html=$timetable_create->timetable_html($date,$view_user_id);
+        $timetable_create = new TimetableCreate;
+        $timetable_html = $timetable_create->timetable_html($date, $view_user_id);
 
-        $member_btn_html=$user->member_list_btn($session_user_id,$session_room_id);
+        $member_btn_html = $user->member_list_btn($session_user_id, $session_room_id);
         
-        return view('timetable',["timetable_html"=>$timetable_html,
-                                 "user_id"=>$session_user_id,
-                                 "view_username"=>$view_username,
-                                 "view_user_id"=>$view_user_id,
-                                 "room_id"=>$room_id,
-                                 "member_btn_html"=>$member_btn_html]);
+        return view('timetable', ["timetable_html"=>$timetable_html,
+                                  "user_id"=>$session_user_id,
+                                  "view_username"=>$view_username,
+                                  "view_user_id"=>$view_user_id,
+                                  "room_id"=>$room_id,
+                                  "member_btn_html"=>$member_btn_html]);
     }
     public function timetable(Request $request,$view_user_id,$room_id){
 
         //member_btnで表示させようとした時
         if(isset($request->member_btn)){
-            $display_date="today";
-            $view_user_id=$request->member_btn;
+            $display_date = "today";
+            $view_user_id = $request->member_btn;
         }
 
         if($request->datepicker){
-            $display_date=$request->datepicker;
+            $display_date = $request->datepicker;
         }
         
         return redirect(url('/timetable/'.$view_user_id.'/'.$room_id.'/'.$display_date));
     }
     public function timetable_delete(Request $request){
         //dd($request->room_id);
-        $schedule=new Schedule;
+        $schedule = new Schedule;
         $schedule->schedule_delete($request->delete_id);
 
         return redirect(url('/timetable/'.$request->view_user_id.'/'.$request->room_id.'/'.$request->display_date));
