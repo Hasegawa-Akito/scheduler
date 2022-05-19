8aa904f1ebfa1578cfb0f7e5f979dd15e673647c
Hasegawa-Akito <h.akito1999@gmail.com>
2022-05-19 23:53:51 +0900

    修正

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,7 +12,7 @@ class User extends Authenticatable
 {
     use HasFactory, Notifiable;
 
-    private $member_btn_html="";
+    private $member_btn_html = "";
 
     /**
      * The attributes that are mass assignable.
@@ -45,33 +45,33 @@ class User extends Authenticatable
     ];
 
     public function user_create($user_info){
-        $user=new User;
-        $user->password=$user_info['password'];
-        $user->username=$user_info['username'];
-        $user->room_id=$user_info['room_id'];
+        $user = new User;
+        $user->password = $user_info['password'];
+        $user->username = $user_info['username'];
+        $user->room_id = $user_info['room_id'];
 
         return $user->save();
     }
     public function user_serch($user_info){
-        $user=User::where('room_id',$user_info["room_id"])
-                    ->where('username',$user_info["username"])
+        $user = User::where('room_id', $user_info["room_id"])
+                    ->where('username', $user_info["username"])
                     ->first();
-        if($user&&(!password_verify($user_info["password"], $user->password))){
-            $user=false;
+        if($user && (!password_verify($user_info["password"], $user->password))){
+            $user = false;
         }
         return $user;
     }
     public function user_id_pass_serch($user_info){
-        $user=User::where('user_id',$user_info["user_id"])
+        $user = User::where('user_id', $user_info["user_id"])
                     ->first();
-        if($user&&(!password_verify($user_info["password"], $user->password))){
-            $user=false;
+        if($user && (!password_verify($user_info["password"], $user->password))){
+            $user = false;
         }
         return $user;
     }
     public function user_id_serch($user_id, $room_id){
-        $user=User::where('user_id',$user_id)
-                    ->where('room_id',$room_id)
+        $user = User::where('user_id', $user_id)
+                    ->where('room_id', $room_id)
                     ->first();
                     
         //dd($user);
@@ -80,10 +80,11 @@ class User extends Authenticatable
     }
 
     public function user_room_id_serch($room_id){
-        $user=User::where('room_id',$room_id)
+        $user = User::where('room_id', $room_id)
                     ->get();
         return $user;
     }
+
 
     public function member_list_btn($user_id,$room_id){
 
@@ -94,8 +95,9 @@ class User extends Authenticatable
         
         //メンバーボタン設置
         $members=User::where('room_id',$room_id)->get();
+
         foreach($members as $member){
-            $this->member_btn_html.= <<< EOS
+            $this->member_btn_html .= <<< EOS
                 <button type="submit" class="btn btn-outline-primary ml-1" value="$member->user_id" name="member_btn">$member->username</button>
             EOS;
         }
